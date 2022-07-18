60163ace03cb96a640dd524242a702917de96f81
inkoz <inkoz@DESKTOP-JS5JFS1>
2022-07-18 10:30:26 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    internal class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            Work54 work54 = new Work54();
+            work54.Work54_code();
+
+            Work56 work56 = new Work56();   
+            work56.Work56_code();
+
+            Work58 Work58 = new Work58();
+            Work58.Work58_Code();
+
+            Work60 Work60 = new Work60();   
+            Work60.Work60_code();
+
+            Work62 Work62 = new Work62();
+            Work62.Work62_Code();
+
+        }
+    }
+}
